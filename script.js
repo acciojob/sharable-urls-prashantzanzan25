7c9ff386df84105a6f5e2f1b0ecd404267c334b7
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-06 19:14:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-// your code here
-const nameInput = document.getElementById("name");
-const yearInput = document.getElementById("year");
-const button = document.getElementById("button");
-const urlElement = document.getElementById("url");
 
-Add EventListner('check', ()=>{
-	const name = nameInput.value;
-	const year = yearInput.value;
-
-	const queryString = `?name=${encodeURIComponent(name)}&year=${year}`;
-	
-	urlElement.textContent = `https://localhost:8080/${queryString}`;
-});
 
